b99ad0c9d54aa3595da0fdcf65cbf9a31252cfa7
Sérgio R Oliveira <111983762+sergioro007@users.noreply.github.com>
2022-11-29 19:45:31 -0300

    Add files via upload
    Exemplo de utilização do "case"

---
+++ main.py
@@ -0,0 +1,14 @@
+print("=================MENU ==================")
+print("1 - Voar\n2 - Não Morrer\n3 - Ficar Rico\n4 - Ser o Flash")
+print("")
+opcao = int(input("O que você deseja?"))
+
+match opcao:
+    case 1:
+        print("Voa, passarinho!")
+    case 2:
+        print("Você imortal!")
+    case 3:
+        print("Parabéns, você será bajulado para sempre")
+    case 4:
+        print("Cadê tu? Vuuuuuuuuu")
